fdf73d6af05e46f8ffe720de864e0be83c11fe8b
Ayush-env <dayush922@gmail.com>
2026-06-17 22:52:40 +0530

    Basic pattern problems

---
+++ pattern.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int i,j;
+  for(i = 0; i < 3; i++){
+    for(j = 1; j <= 4; j++){
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
